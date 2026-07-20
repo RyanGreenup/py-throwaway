2a1fe3ac2ea397a37bbd1fbe8becd466f75b91b1
Ryan Greenup <exogenesis@protonmail.com>
2026-07-20 11:00:24 +1000

    feat: fix negatives

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def fib(n: int) -> int:
     # base case
     if n < 0:
-        raise ValueError("No negatives here today")
+        raise ValueError("Negatives not handled")
     if n == 0:
         return 0
     if n == 1:
