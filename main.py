cb11f9f4876c223402377a7318690cc190306ace
Ryan Greenup <exogenesis@protonmail.com>
2026-07-20 10:58:05 +1000

    initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+def fib(n: int) -> int:
+    # base case
+    if n < 0:
+        raise ValueError("No negatives here today")
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return fib(n - 1) + fib(n - 2)
+
+
+def main() -> None:
+    # F = fib(-1)
+    # print(F)
+    F = fib(0)
+    print(F)
+    F = fib(1)
+    print(F)
+    F = fib(2)
+    print(F)
+    F = fib(3)
+    print(F)
+    F = fib(4)
+    print(F)
+    F = fib(5)
+    print(F)
+
+
+if __name__ == "__main__":
+    main()
